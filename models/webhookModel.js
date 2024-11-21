5ba2988b86623fc9703c75b306ee220a5673a18a
pawnesh <pawneshkitio@gmail.com>
2024-11-21 13:20:42 +0530

    Adjusted model schema

--- models/webhookModel.js
+++ models/webhookModel.js
@@ -1,16 +1,11 @@
 const mongoose = require('mongoose');
 const leadsDataSchema = new mongoose.Schema({
-    object: String,
-    entry: [{
-      id: String,
       name: String,
       from: String,
       message_id: String,
       timestamps: String,
       type: String,
       message: String
-
-    }]
   }, { timestamps: true });
   
   module.exports = mongoose.model('leadsData',leadsDataSchema);
