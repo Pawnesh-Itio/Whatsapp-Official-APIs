1205be161ed51b04582c6ec689b5a11c50dda4d3
pawnesh <pawneshkitio@gmail.com>
2024-11-21 15:24:10 +0530

    Debugging

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -100,62 +100,64 @@ const receiveMessage = async (req, res) => {
       for (const entry of data.entry) {
         const changes = entry.changes;
         // Iterate over each change
+        console.log(changes);
         for (const change of changes) {
-          if (change.field === 'messages') {
-            const message = change.value.messages[0];
-            const contacts = change.value.contacts[0];
-            console.log('Received message:', message);
+          console.log(changes);
+          // if (change.field === 'messages') {
+          //   const message = change.value.messages[0];
+          //   const contacts = change.value.contacts[0];
+          //   console.log('Received message:', message);
 
-            // Initialize form data
-                const formData = new FormData();
-                formData.append('name', contacts.profile.name);
-                formData.append('from', message.from);
-                formData.append('id', message.id);
-                formData.append('timestamp', message.timestamp);
-                formData.append('text', message.text.body);
-                formData.append('type', message.type);
+          //   // Initialize form data
+          //       const formData = new FormData();
+          //       formData.append('name', contacts.profile.name);
+          //       formData.append('from', message.from);
+          //       formData.append('id', message.id);
+          //       formData.append('timestamp', message.timestamp);
+          //       formData.append('text', message.text.body);
+          //       formData.append('type', message.type);
 
-            // Initializing mongoss data
-                const documentToInsert = {
-                  name: contacts.profile.name,
-                  from: message.from,
-                  message_id: message.id,
-                  timestamp: message.timestamp,
-                  type:message.type,
-                  message:message.text.body
-                };
-                try{
-                  const leadData = await WebhookData.findOne({ from: documentToInsert.from });
-                  if(leadData){
-                    console.log("Lead Exist: Add socket logic here...");
-                    const io = req.app.get('io');  // Get the Socket.io instance
-                    io.emit('newMessageNotification', { documentToInsert, message: 'Lead exists, update or notify.' });
+          //   // Initializing mongoss data
+          //       const documentToInsert = {
+          //         name: contacts.profile.name,
+          //         from: message.from,
+          //         message_id: message.id,
+          //         timestamp: message.timestamp,
+          //         type:message.type,
+          //         message:message.text.body
+          //       };
+          //       try{
+          //         const leadData = await WebhookData.findOne({ from: documentToInsert.from });
+          //         if(leadData){
+          //           console.log("Lead Exist: Add socket logic here...");
+          //           const io = req.app.get('io');  // Get the Socket.io instance
+          //           io.emit('newMessageNotification', { documentToInsert, message: 'Lead exists, update or notify.' });
 
-                  }else{
-                    console.log("Lead Not Exist: Add Insertion logic here");
-                    const newLead = new WebhookData(documentToInsert);
-                    await newLead.save();
-                     // Log the form data.
-                     console.log('Form Data :', formData);
-                     // Sending POST request to an external API endpoint with form-data
-                     const response = await axios.post('https://xeyso.com/crm/wa-server', formData, {
-                      headers: {
-                        'Content-Type': 'multipart/form-data' // Ensure the request is sent as form-data
-                        }
-                      });
-                      const { message, status } = response.data;
-                      // Log the response from the external API
-                      console.log('Response from external API:',` Message: ${message} Status: ${status}`);
-                      // Revert to the person with automated message.
+          //         }else{
+          //           console.log("Lead Not Exist: Add Insertion logic here");
+          //           const newLead = new WebhookData(documentToInsert);
+          //           await newLead.save();
+          //            // Log the form data.
+          //            console.log('Form Data :', formData);
+          //            // Sending POST request to an external API endpoint with form-data
+          //            const response = await axios.post('https://xeyso.com/crm/wa-server', formData, {
+          //             headers: {
+          //               'Content-Type': 'multipart/form-data' // Ensure the request is sent as form-data
+          //               }
+          //             });
+          //             const { message, status } = response.data;
+          //             // Log the response from the external API
+          //             console.log('Response from external API:',` Message: ${message} Status: ${status}`);
+          //             // Revert to the person with automated message.
                       
-                      // Send the response back to the client
-                      return res.json(response.data); // Send response and stop further processing
-                  }
-                } catch (err) {
-                  console.error('Error:', err);
-                  res.status(500).send('Server error');
-                }
-          }
+          //             // Send the response back to the client
+          //             return res.json(response.data); // Send response and stop further processing
+          //         }
+          //       } catch (err) {
+          //         console.error('Error:', err);
+          //         res.status(500).send('Server error');
+          //       }
+          // }
         }
       }
       // If no valid message data is processed, send a 200 status
