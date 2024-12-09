4e97ad6d12884c7f857ab22d2b0dc2a2ebaea178
pawnesh <pawneshkitio@gmail.com>
2024-12-09 15:42:40 +0530

    updated chat fetching function

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -13,9 +13,17 @@ const messageList = async (req, res) =>{
     const {chatId} = req.params
     try{
     const findContactData = await contactData.findOne({ wa_phone_number: chatId});
+    if(findContactData){
     const contactId = findContactData._id;
     const messages = await messageModel.find({contactId: contactId}); 
-    return res.status(200).json({messages: messages, status: "success", contaact: findContactData});
+        if(messages){
+            return res.status(200).json({messages: messages, status: "success", contaact: findContactData});
+        }else{
+            return res.status(200).json({ status: "no_messages", contaact: findContactData});
+        }
+    }else{
+        return res.status(200).json({ status: "no_contact"});
+    }
     } catch (error) {
         console.error('Error fetching contacts:', error);
         res.status(500).json({ error: 'An error occurred while fetching contacts' });
